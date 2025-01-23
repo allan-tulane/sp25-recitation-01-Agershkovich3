497888f54c4076c9db06aec794b2eeb299ca431b
Agershkovich3 <166247073+Agershkovich3@users.noreply.github.com>
2025-01-23 15:35:29 +0000

    trying stuff to solver autograder error

--- test_main.py
+++ test_main.py
@@ -1,21 +1,24 @@
+from main import linear_search
+from main import binary_search
+from main import compare_search
 from main import *
 import tabulate
 
 
 def test_linear_search():
 	""" done. """
-	assert linear_search([1,2,3,4,5], 5) == 4
-	assert linear_search([1,2,3,4,5], 1) == 0
-	assert linear_search([1,2,3,4,5], 6) == -1
+	assert linear_search([1, 2, 3, 4, 5], 5) == 4
+	assert linear_search([1, 2, 3, 4, 5], 1) == 0
+	assert linear_search([1, 2, 3, 4, 5], 6) == -1
 
 
 def test_binary_search():
-	assert binary_search([1,2,3,4,5], 5) == 4
-	assert binary_search([1,2,3,4,5], 1) == 0
-	assert binary_search([1,2,3,4,5], 6) == -1
-	assert binary_search([1,2,3,4,5,7,8,11,20,23,31,42], 23) == 9
-	assert binary_search([1,2,3,4,5,7,8,11,20,23,31,42], 8) == 6
-	assert binary_search([1,2,3,4,5,7,8,11,20,23,31,42], 21) == -1
+	assert binary_search([1, 2, 3, 4, 5], 5) == 4
+	assert binary_search([1, 2, 3, 4, 5], 1) == 0
+	assert binary_search([1, 2, 3, 4, 5], 6) == -1
+	assert binary_search([1, 2, 3, 4, 5, 7, 8, 11, 20, 23, 31, 42], 23) == 9
+	assert binary_search([1, 2, 3, 4, 5, 7, 8, 11, 20, 23, 31, 42], 8) == 6
+	assert binary_search([1, 2, 3, 4, 5, 7, 8, 11, 20, 23, 31, 42], 21) == -1
 	### TODO: add two more tests here.
 
 	###
